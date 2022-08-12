0684550d2f23c78de92e374645dd82ff2264a7df
Malcolm L <90416865+laforcem@users.noreply.github.com>
2022-08-11 21:55:53 -0600

    tailwind stuff

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use bootstrap
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
